6368c0a924cd0be37b03c3feac07424ebd008f15
Zeba Fathima <fathimazeba882@gmail.com>
2026-06-24 12:17:01 +0530

    Use environment variable for server port
    Update app.run to use environment variable for port.

--- backend/app.py
+++ backend/app.py
@@ -276,4 +276,5 @@ def reset_job(job_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
